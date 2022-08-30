5f471db5b3893714cfbf36bc4122da44b0d72dc5
ZeroRin <pptxp@qq.com>
2022-08-30 17:06:25 +0800

    remove submodule dependency

--- pack_resdata.py
+++ pack_resdata.py
@@ -1,11 +1,14 @@
 import zipfile
 import os
 from pathlib import Path
+from urllib import request
 os.chdir(Path(__file__).resolve().parent)
 
 f = zipfile.ZipFile('resdata.zip','w')
-data_dir = '../data'
+os.makedirs('./resdata',exist_ok=True)
 regions = ['ch','jp','tw','us','kr']
 for region in regions:
-    f.write(os.path.join(data_dir,region,'resdata.json'),f'{region}_resdata.json',compress_type=zipfile.ZIP_BZIP2)
+    url = f'https://github.com/gf-data-tools/gf-data-{region}/raw/main/resdata.json'
+    request.urlretrieve(url,f'./resdata/{region}_resdata.json')
+    f.write(f'./resdata/{region}_resdata.json',f'{region}_resdata.json',compress_type=zipfile.ZIP_BZIP2)
 
